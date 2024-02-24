7e1a513ef4d658fc93d004b31f36d31bfb75d705
Oreoff <maximmelnik37@gmail.com>
2024-02-24 12:36:13 +0200

    Created class & added datasource

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,13 +3,24 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import ua.edu.lntu.cw3.ui.theme.IPZ_CR_3Theme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +42,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+
     Text(
         text = "Hello $name!",
         modifier = modifier
--- app/src/main/java/ua/edu/lntu/cw3/data/Datasource.kt
+++ app/src/main/java/ua/edu/lntu/cw3/data/Datasource.kt
@@ -1,4 +1,12 @@
 package ua.edu.lntu.cw3.data
-
+import ua.edu.lntu.cw3.model.CardText
 class Datasource {
+    fun loadCards(): List<CardText> {
+        return listOf<CardText>(
+            CardText("Lorem ipsum","Lorem Ipsum is simply dummy text of the printing and typesetting industry."),
+            CardText("Never gonna give you up","Never gonna let you down" ),
+            CardText("Ipsum","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." )
+        )
+    }
+
 }
